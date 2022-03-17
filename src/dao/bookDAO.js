befa5f5568d20169c35afb1207f421a0592c2ac1
vanessacreis <vanessacreis@live.com>
2022-03-16 22:54:30 -0300

    Criação da estrutura da DAO

---
+++ src/dao/bookDAO.js
@@ -0,0 +1,15 @@
+class BookDAO {
+  constructor(database) {
+    this.database = database;
+  }
+
+  allBooks = () => {};
+
+  showOneBook = (name) => {};
+
+  insertBook = (name) => {};
+
+  deleteBook = (name) => {};
+}
+
+export default BookDAO;
