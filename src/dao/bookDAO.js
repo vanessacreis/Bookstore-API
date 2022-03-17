541beedfa36fd4d7864acbd2f0189290dc22fc18
vanessacreis <vanessacreis@live.com>
2022-03-17 15:04:04 -0300

    Inserção das querys na DAO

--- src/dao/bookDAO.js
+++ src/dao/bookDAO.js
@@ -1,15 +1,122 @@
 class BookDAO {
   constructor(database) {
-    this.database = database;
+    this.db = database;
   }
 
-  allBooks = () => {};
+  allBooks = () => {
+    return new Promise((resolve, reject) => {
+      this.db.all("SELECT * FROM BOOKS", (error, rows) => {
+        if (error) {
+          reject({
+            message: error.message,
+            error: true,
+          });
+        } else {
+          resolve({
+            books: rows,
+            error: false,
+          });
+        }
+      });
+    });
+  };
 
-  showOneBook = (name) => {};
+  showOneBook = (name) => {
+    return new Promise((resolve, reject) => {
+      this.db.all("SELECT * FROM BOOKS WHERE NAME = ?", name, (error, rows) => {
+        if (error) {
+          reject({
+            message: error.message,
+            erro: true,
+          });
+        } else {
+          resolve({
+            book: rows,
+            erro: false,
+          });
+        }
+      });
+    });
+  };
 
-  insertBook = (name) => {};
+  insertBook = (newBook) => {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        "INSERT INTO BOOKS(NAME, WRITER, PUBLISHER, GENRE, PAGES, LANGUAGE, YEAR, PRICE) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        newBook.name,
+        newBook.writer,
+        newBook.publisher,
+        newBook.genre,
+        newBook.pages,
+        newBook.language,
+        newBook.year,
+        newBook.price,
+        (error) => {
+          if (error) {
+            reject({
+              message: error.message,
+              error: true,
+            });
+          } else {
+            resolve({
+              message: `Book ${newBook.name} registered successfully`,
+              book: newBook,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
 
-  deleteBook = (name) => {};
+  deleteBook = (id) => {
+    return new Promise((resolve, reject) => {
+      this.db.run("DELETE FROM BOOK WHERE ID = ?", id, (error) => {
+        if (error) {
+          reject({
+            message: error.message,
+            erroe: true,
+          });
+        } else {
+          resolve({
+            book: `Book nº${id} deleted.`,
+            erro: false,
+          });
+        }
+      });
+    });
+  };
+
+  updateBook = (id, book) => {
+    return new Promise((resolve, reject) => {
+      this.db.run(
+        "UPDATE BOOKS SET NAME = ?, WRITER = ?, PUBLISHER = ?, GENRE = ?, PAGES = ?, LANGUAGE = ?, YEAR = ?, PRINCE = ? WHERE ID = ?",
+        book.name,
+        book.writer,
+        book.publisher,
+        book.genre,
+        book.pages,
+        book.language,
+        book.year,
+        book.price,
+        id,
+        (error) => {
+          if (error) {
+            reject({
+              message: error.message,
+              error: true,
+            });
+          } else {
+            resolve({
+              message: `Book ${id} updated.`,
+              book: book,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
 }
 
 export default BookDAO;
