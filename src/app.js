e0dd783df17df12267e486d8d15a807445fb3a2a
vanessacreis <vanessacreis@live.com>
2022-03-18 12:20:16 -0300

    Importando controller e database no servidor

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import bookController from "./controller/book-controller.js";
+import database from "./database/sqlite-db.js";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+bookController(app, database);
 
 export default app;
